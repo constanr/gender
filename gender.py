f9a0fa61d1ccb1c143eaa91415d0709e46b372ad
croman <constanromangomez@hotmail.com>
2016-02-05 14:17:30 +0100

    Now the model classifies each profile instead of each tweet

--- gender.py
+++ gender.py
@@ -9,38 +9,27 @@ from sklearn import cross_validation, svm, linear_model, tree, ensemble, naive_b
 #pos_tags = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS',
 #            'PDT', 'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'SYM', 'TO', 'UH', 'VB', 'VBD', 'VBG',
 #            'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$', 'WRB']
-EMOTICONS = r"""
-    (?:
-      [<>]?
-      [:;=8]                     # eyes
-      [\-o\*\']?                 # optional nose
-      [\)\]\(\[dDpP/\:\}\{@\|\\] # mouth
-      |
-      [\)\]\(\[dDpP/\:\}\{@\|\\] # mouth
-      [\-o\*\']?                 # optional nose
-      [:;=8]                     # eyes
-      [<>]?
-      |
-      <3                         # heart
-    )"""
-#EMOTICON_RE = re.compile(EMOTICONS, re.VERBOSE | re.I | re.UNICODE)
-smileys = """:-) :) :o) :] :3 :c) :> =] 8) =) :} :^)
-             :D 8-D 8D x-D xD X-D XD =-D =D =-3 =3 B^D""".split()
-pattern2 = "|".join(map(re.escape, smileys))
+
 def xmltojson(file, gender):
 
     posts = []
-    texts = []
-    genders = []
+    texts = ''
     tree = et.parse(file)
     documents = tree.iterfind('document')
-    for d in documents:
+    """for d in documents:
         post = {}
         post['text'] = d.text
         post['gender'] = gender
-        posts.append(post)
+        posts.append(post)"""
+    for d in documents:
+        texts += d.text+'\n'
+    post = {}
+    post['text'] = texts
+    post['gender'] = gender
+    posts.append(post)
 
     return posts
+    #return post
 
 def tweetstojson():
 
@@ -63,24 +52,41 @@ def tweetstojson():
         tweets = json.dumps(tweets)
         dataset.write(tweets)
 
-def tagging(path, posts):
+def features(path, posts):
     gender = []
     pos_tags = []
     tag_count = []
+    with open('EmoticonSentimentLexicon.txt') as emoticon_lexicon:
+        lines = emoticon_lexicon.readlines()
+        emot_dict = {}
+        for l in lines:
+            line = l.split('\t')
+            emot_dict[line[0]] = int(line[1].split('\r')[0])
 
     for p in posts:
         text = p['text']
-        all_emoticons = re.findall(pattern2, text)
-        print text
-        print all_emoticons
-        caps = ('CAPS', float(sentiment_feature_extraction.caps_words(text))/len(text.split()))
-        elongated = ('ELONGATED', float(sentiment_feature_extraction.elonganted_words(text))/len(text.split()))
-        emoticon_count = ('EMOTCOUNT', len(all_emoticons))
+        emoticons = sentiment_feature_extraction.emoticons_from_dictionary(text, emot_dict)
+        if text.split()<=0.0:
+            length = len(text.split())
+        else:
+            length = 1.0
+        caps = ('CAPS', float(sentiment_feature_extraction.caps_words(text))/length)
+        elongated = ('ELONGATED', float(sentiment_feature_extraction.elonganted_words(text))/length)
+        exclamation_interrogation_dict = sentiment_feature_extraction.exclamation_and_interrogation(text)
+        excl = ('!', exclamation_interrogation_dict['!'])
+        interr = ('?', exclamation_interrogation_dict['?'])
+        omg_count = ('OMG', len(re.findall('omg', text, re.I)))
+        heart_count = ('<3', len(re.findall('<3', text)))
+        lol_count = ('lol', len(re.findall('lo+l', text, re.I)))
+        lmfao_count = ('lmfao', len(re.findall('lmfa+o+', text, re.I)))
+        emoticon_count = ('EMOTCOUNT', emoticons['number_emoticons'])
+        emoticon_score = ('EMOTSCORE', emoticons['score_emoticons'])
         count = count_pos(text)
-        #count.extend((caps, elongated, emoticon_count))
+        count.extend((caps, elongated, excl, interr, emoticon_count, emoticon_score, heart_count, omg_count, lol_count, lmfao_count))
         tag_count.append(count)
         #print caps[1], elongated[1], emoticon_count[1]
         gender.append(p['gender'])
+        print len(posts)
 
     pos_train, pos_test, gender_train, gender_test = cross_validation.train_test_split(
             tag_count, gender, test_size=0.1, random_state=0)
@@ -137,21 +143,21 @@ def gender_identification(file, tagging, format):
             posts = json.load(json_file)
 
     if tagging:
-        tagging(path, posts)
-    else:
-        with open(path+'training-tags.txt', 'r') as training_file, open(path+'test-tags.txt', 'r') as test_file, open(path+'gender-train.txt', 'r') as gender_train_file, open(path+'gender-test.txt', 'r') as gender_test_file:
-            complete_tag_count = []
-            test_tag_count = []
-            gender_train = []
-            gender_test = []
-            for line in training_file:
-                complete_tag_count = json.loads(line)
-            for line in test_file:
-                test_tag_count = json.loads(line)
-            for line in gender_train_file:
-                gender_train = json.loads(line)
-            for line in gender_test_file:
-                gender_test = json.loads(line)
+        features(path, posts)
+
+    with open(path+'training-tags.txt', 'r') as training_file, open(path+'test-tags.txt', 'r') as test_file, open(path+'gender-train.txt', 'r') as gender_train_file, open(path+'gender-test.txt', 'r') as gender_test_file:
+        complete_tag_count = []
+        test_tag_count = []
+        gender_train = []
+        gender_test = []
+        for line in training_file:
+            complete_tag_count = json.loads(line)
+        for line in test_file:
+            test_tag_count = json.loads(line)
+        for line in gender_train_file:
+            gender_train = json.loads(line)
+        for line in gender_test_file:
+            gender_test = json.loads(line)
 
     complete_tag_count = np.array(complete_tag_count).astype(np.float)
     test_tag_count = np.array(test_tag_count).astype(np.float)
@@ -161,16 +167,16 @@ def gender_identification(file, tagging, format):
     gender_total = np.concatenate((gender_train, gender_test))
 
     clf1 = linear_model.LogisticRegression()
-    clf2 = ensemble.RandomForestClassifier()
-    clf3 = tree.DecisionTreeClassifier(max_depth=4)
-    clf4 = neighbors.KNeighborsClassifier(n_neighbors=7)
-    clf5 = svm.SVC(probability=True)
+    clf2 = ensemble.RandomForestClassifier(n_estimators=100)
+    clf3 = tree.DecisionTreeClassifier(max_depth=5)
+    clf4 = neighbors.KNeighborsClassifier(n_neighbors=10)
+    clf5 = svm.SVC(kernel='linear', probability=True)
     eclf = ensemble.VotingClassifier(estimators=[('lr', clf1), ('rf', clf2), ('dt', clf3), ('kn', clf4),
-                                                 ('svc', clf5)], voting='soft')
+                                                 ('svc', clf5)], voting='hard')
     cv = cross_validation.ShuffleSplit(tag_total.shape[0], n_iter=3, test_size=0.3, random_state=0)
 
     for clf, label in zip([clf1, clf2, clf3, clf4, clf5, eclf], ['Logistic Regression', 'Random Forest',
-                                                     'Decission Tree', 'KNeighbors', 'SVC', 'Ensemble']):
+                                                     'Decision Tree', 'KNeighbors', 'SVC', 'Ensemble']):
         scores = cross_validation.cross_val_score(clf, tag_total, gender_total, cv=cv, scoring='accuracy')
         print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
 
@@ -179,8 +185,7 @@ def gender_identification(file, tagging, format):
     #clf.fit(complete_tag_count, gender_train)
     #print clf.score(test_tag_count, gender_test)
 
-
-#print gender_identification('data/blog/blog-gender-dataset.json', False, 'json')
-#print gender_identification('data/blog/small-dataset.json', False, 'json')
+#print gender_identification('data/blog/blog-gender-dataset.json', True, 'json')
+#print gender_identification('data/blog/small-dataset.json', True, 'json')
 print gender_identification('data/pan15-english/dataset.json', False, 'json')
 
